b652ef2521bbf3be72aebad959279bf04022bfba
anareaty <reatymain@gmail.com>
2024-08-16 00:01:48 +0400

    Add files to folders

--- main.ts
+++ main.ts
@@ -1,20 +1,81 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, SuggestModal, setIcon, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, SuggestModal, FuzzySuggestModal, TAbstractFile, TFile, setIcon, Setting } from 'obsidian';
+import { FolderSuggest} from 'suggesters/suggestor';
+
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	selectedGroupName: string;
+interface GroupToFolder {
+	group: string;
+	folder: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	selectedGroupName: '-'
+interface MyPluginSettings {
+	selectedGroupName: string;
+	linkGroupsToFolders: GroupToFolder[];
 }
+
+
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	selectedGroupName: '-',
+	linkGroupsToFolders: []
+}
+
+
+
+class SelectFileSuggest extends FuzzySuggestModal<TFile> {
+
+	plugin: MyPlugin;
+
+
+  constructor(app: App, plugin: MyPlugin) {
+    super(app);
+	this.plugin = plugin;
+  }
+
+	getItems(): TFile[] {
+		const abstractFiles = this.app.vault.getAllLoadedFiles();
+		const files: TFile[] = [];
+
+		abstractFiles.forEach((file: TAbstractFile) => {
+			if (file instanceof TFile) {
+				files.push(file);
+			}
+		});
+
+		return files;
+	}
+
+	getItemText(file: TFile) {
+		return file.path
+	}
+
+	renderSuggestion(match: any, el: HTMLElement) {
+		let path = match.item.path.replace(".md", "")
+		el.createEl("div", {text: path})
+	}
+
+	async onChooseItem(file: TFile, evt: MouseEvent) {
+		await this.plugin.addSelectedFile(file)
+		
+		this.close();
+	}
+
+	
+}
+
+
+
+
+
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+
+		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		this.app.workspace.onLayoutReady(async () => {
 			let leaf = this.app.workspace.getLeavesOfType("bookmarks")[0]
@@ -34,14 +95,32 @@ export default class MyPlugin extends Plugin {
 					});
 				}
 
+
+
+				// @ts-ignore
+				let noteSelectButtonNode = Array.from(navButtonsContainer.childNodes).find(n => n.classList.contains("note-select-icon"))
+
+				if (!noteSelectButtonNode) {
+					let noteSelectButton = navButtonsContainer?.createEl("div", { text: "", cls: "clickable-icon nav-action-button note-select-icon" })
+					setIcon(noteSelectButton, "scan-search")
+
+					this.registerDomEvent(noteSelectButton, 'click', async () => {
+						new SelectFileSuggest(this.app, this).open()
+					});
+				}
+
+
+
+
+
 				// @ts-ignore
 				let plusButtonNode = Array.from(navButtonsContainer.childNodes).find(n => n.classList.contains("bookmark-plus-icon"))
 
 				if (!plusButtonNode) {
-					let groupSelectButton = navButtonsContainer?.createEl("div", { text: "", cls: "clickable-icon nav-action-button bookmark-plus-icon" })
-					setIcon(groupSelectButton, "plus")
+					let plusButton = navButtonsContainer?.createEl("div", { text: "", cls: "clickable-icon nav-action-button bookmark-plus-icon" })
+					setIcon(plusButton, "plus")
 
-					this.registerDomEvent(groupSelectButton, 'click', async () => {
+					this.registerDomEvent(plusButton, 'click', async () => {
 						await this.addNoteForGroup()
 					});
 				}
@@ -71,118 +150,144 @@ export default class MyPlugin extends Plugin {
 
 
 
+
+
+	async addSelectedFile(file: TFile) {
+		await this.loadSettings()
+		let selectedGroupName = this.settings.selectedGroupName
+		let group = this.getBookmarkGroupByName(selectedGroupName)
+		await this.addFileToBoolmarkGroup(file, group)
+	}
+
+
+
 	async addNoteForGroup() {
 		await this.loadSettings()
 		let selectedGroupName = this.settings.selectedGroupName
+		let linkGroupsToFolders = this.settings.linkGroupsToFolders
+		let groupToFolder = linkGroupsToFolders.find(l => l.group == selectedGroupName)
 
 		// @ts-ignore
 		let noteFolder = this.app.vault.config.newFileFolderPath
 		if (!noteFolder) noteFolder = ""
+
+		if (groupToFolder) {
+			noteFolder = groupToFolder.folder
+		}
+
 		let noteName = "New note"
+		let path = this.checkIfExist(0, noteName, noteFolder)
+		let file: TFile = await this.app.vault.create(path, "")
+		await app.workspace.getLeaf().openFile(file)
+		let group = this.getBookmarkGroupByName(selectedGroupName)
+		await this.addFileToBoolmarkGroup(file, group)
+	}
 
-		const checkIfExist: any = (num: number) => {
-			let numString = ""
-			if (num > 0) {numString = " " + num}
-			let path = noteName + numString + ".md"
-	
-			if (noteFolder && noteFolder != "") {
-				path = noteFolder + "/" + noteName + numString + ".md"
-			}
-	
-			// @ts-ignore
-			let checkPath = this.app.vault.getAbstractFileByPathInsensitive(path)
 
 	
-			if (checkPath) {
-				return checkIfExist(num + 1)
-			} else return path
+
+
+	getGroupList() {
+		const flattenGroups = (arr: any): any => {
+			var newArr = [];
+				for (let item of arr) {
+				  if (item.type == "group") {
+					  newArr.push(item)
+					  if (item.items) { 
+						let items = item.items.map((i: any) => {
+						  if (!item.parent) item.parent = ""
+						  i.parent = item.parent + item.title + "/"
+						  return i
+						})
+						newArr = newArr.concat(flattenGroups(items))
+					  }
+				  }
+				}
+			return newArr;
 		}
+		// @ts-ignore
+		let groups = this.app.internalPlugins.plugins.bookmarks.instance.items
 
-		let path = checkIfExist(0)
+		groups = groups.map((g:any) => {
+			g.parent = ""
+			return g
+		})
+
+		groups = flattenGroups(groups)
+		return groups
+	}
+
+
+	getBookmarkGroupByName(selectedGroupName: string) {
+		let groups = this.getGroupList()
+		let group = groups.find((i: any) => i.parent + i.title == selectedGroupName)
+		return group
+	}
 
 
 
 
 
-		await this.app.vault.create(path, "")
-
-		let fileObj = {
-			type: "file",
-			path: path
-		}
-
+	async addFileToBoolmarkGroup(file: TFile, group: any) {
 		// @ts-ignore
 		let instance = this.app.internalPlugins.plugins.bookmarks.instance
-
-		
-
-		
-
-		let groups = this.flattenGroups(instance.items)
-
-		
-		
-
-		let group = groups.find((i: any) => i.parent + i.title == selectedGroupName)
-
 
 		let bookmark
 
 		if (group) {
-			bookmark = group.items.find((i: any) => i.path == path)
+			bookmark = group.items.find((i: any) => i.path == file.path)
 		} else {
-			bookmark = instance.items.find((i: any) => i.path == path)
+			bookmark = instance.items.find((i: any) => i.path == file.path)
 		}
 		
-
-		
-
-
-		
-
 		if (!bookmark) {
+			
+			let fileObj = {
+				type: "file",
+				path: file.path
+			}
+
 			if (group) {
 				await instance.addItem(fileObj, group)
 			} else {
 				await instance.addItem(fileObj)
 			}
+		}
+	}
 
+
+
+
+
+
+	checkIfExist(num: number, noteName: string, noteFolder: string): any {
+		let numString = ""
+		if (num > 0) {numString = " " + num}
+		let path = noteName + numString + ".md"
+
+		if (noteFolder && noteFolder != "") {
+			path = noteFolder + "/" + noteName + numString + ".md"
 		}
 
+		// @ts-ignore
+		let checkPath = this.app.vault.getAbstractFileByPathInsensitive(path)
 
 
-
-
+		if (checkPath) {
+			return this.checkIfExist(num + 1, noteName, noteFolder)
+		} else return path
 	}
 
 
-	flattenGroups(arr: any): any {
-		var newArr = [];
-			for (let item of arr) {
-			  if (item.type == "group") {
-				  newArr.push(item)
-				  if (item.items) {
-					  
-					let items = item.items.map((i: any) => {
-					  if (!item.parent) item.parent = ""
-					  i.parent = item.parent + item.title + "/"
-					  return i
-					})
-					newArr = newArr.concat(this.flattenGroups(items))
-				  }
-			  }
-			}
-			return newArr;
-	}
+
 
 
 	async selectBookmarksGroup() {
 
 
-		// @ts-ignore
-		let groups = this.app.internalPlugins.plugins.bookmarks.instance.items
+		let groups = this.getGroupList()
 
-		groups = this.flattenGroups(groups)
+
 		groups = groups.map((g:any) => {
 			const getPath = (g:any) => {
 				let title = g.parent + g.title
@@ -225,6 +330,16 @@ export default class MyPlugin extends Plugin {
 			await this.changeGroup()
 		  }
 	}
+
+
+
+
+	
+
+
+
+
+
 
 
 	async changeGroup() {
@@ -319,4 +434,62 @@ export default class MyPlugin extends Plugin {
 
 
 
+
+
+
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+		containerEl.empty();
+
+		new Setting(containerEl)
+		.setName("Link folders to groups")
+		.setDesc("New notes for groups will be added to the folders")
+
+		let groups = this.plugin.getGroupList()
+
+		for (let group of groups) {
+			let groupPath = group.parent + group.title
+			let linkGroupsToFolders = this.plugin.settings.linkGroupsToFolders
+			let groupToFolder = linkGroupsToFolders.find(l => l.group == groupPath)
+
+			if (!groupToFolder) {
+				groupToFolder = {group: groupPath, folder: ""}
+				linkGroupsToFolders.push(groupToFolder)
+			}
+
+			new Setting(containerEl)
+			.setName(groupPath)
+			.addSearch(search => {
+				new FolderSuggest(search.inputEl, this.app);
+				search				
+				.setPlaceholder('')
+				.setValue(groupToFolder!.folder)
+				.onChange(async (folderPath) => {
+					groupToFolder!.folder = folderPath
+					this.plugin.settings.linkGroupsToFolders = linkGroupsToFolders
+					await this.plugin.saveSettings();
+
+				})
+			});
+
+
+		}
+		
+
+
+		
+			
+
+
+	}
+}
 
